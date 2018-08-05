f1675ad22952d9e73b0a273cf4a4ce203afc16f4
Sven van der Vlist <svenvdvlist@live.nl>
2018-08-05 16:30:13 +0200

    Kata 02 day 03

---
+++ Kata02_KarateChop/Kata02_KarateChop/Karate3.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Kata02_KarateChop
+{
+    public class Karate3 : IKarate
+    {
+        public int Chop(int number, int[] numbers)
+        {
+            if (numbers.Length > 0)
+            {
+                return Chop(number, numbers, 0);
+            }
+
+            return -1;
+        }
+
+        public int Chop(int number, int[] numbers, int index)
+        {
+            if (numbers[index] == number)
+            {
+                return index;
+            }
+
+            if (index + 1 <= numbers.Length - 1)
+            {
+                return Chop(number, numbers, index + 1);
+            }
+
+            return -1;
+        }
+    }
+}
